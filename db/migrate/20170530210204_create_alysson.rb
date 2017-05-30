fade27ceb9805fbd59b4401c503e939f7b31572f
Ricardo Pulice de Almeida <ricardo.pulice@agrodefesa.go.gov.br>
2017-05-30 18:20:19 -0300

    Campo text para evitar conflito entre sqlite e postgres

--- db/migrate/20170530210204_create_alysson.rb
+++ db/migrate/20170530210204_create_alysson.rb
@@ -1,7 +1,8 @@
 class CreateAlysson < ActiveRecord::Migration[5.0]
   def change
+  	drop_table :alyssons
     create_table :alyssons do |t|
-      t.text :pontos, array: true
+      t.text :pontos
       t.timestamps
     end
   end
