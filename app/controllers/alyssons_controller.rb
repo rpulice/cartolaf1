0ac024c3f8cb5f62225a081a26922f8532afbe18
Ricardo Pulice de Almeida <ricardo.pulice@agrodefesa.go.gov.br>
2017-05-30 15:26:30 -0300

    Pontuação F1

--- app/controllers/alyssons_controller.rb
+++ app/controllers/alyssons_controller.rb
@@ -2,7 +2,9 @@ class AlyssonsController < ApplicationController
   before_action :authenticate_user
 
   def index
-	@teams = Team.all
+		@teams = Team.all
+  	client = HTTPClient.new
+		@status = client.get_content('https://api.cartolafc.globo.com/mercado/status')
   end
 
 end
