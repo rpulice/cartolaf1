3917a229fe6060ac3579be1fa8ae72d1644d2b60
Ricardo Pulice de Almeida <ricardopulice@gmail.com>
2017-05-30 21:26:22 -0300

    Tentativa solução heroku

--- app/controllers/alyssons_controller.rb
+++ app/controllers/alyssons_controller.rb
@@ -1,11 +1,12 @@
+require 'json'
+
 class AlyssonsController < ApplicationController
-	require 'json'
   before_action :authenticate_user
 
   def index
 		@teams = Team.all
   	client = HTTPClient.new
 		@status = client.get_content('https://api.cartolafc.globo.com/mercado/status')
-		@alysson = ActiveSupport::JSON.decode(Alysson.first.pontos)
+		@alysson = JSON.parse(Alysson.first.pontos)
   end
 end
