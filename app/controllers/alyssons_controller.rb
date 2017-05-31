2474b4b7e238f79eb38829b525f3073f035202cd
Ricardo Pulice de Almeida <ricardopulice@gmail.com>
2017-05-30 22:21:26 -0300

    Json correção

--- app/controllers/alyssons_controller.rb
+++ app/controllers/alyssons_controller.rb
@@ -5,6 +5,6 @@ class AlyssonsController < ApplicationController
 		@teams = Team.all
   	client = HTTPClient.new
 		@status = client.get_content('https://api.cartolafc.globo.com/mercado/status')
-		@alysson = JSON.parse(Alysson.first.pontos)
+		@alysson = JSON.parse(Alysson.first.pontos.to_json.gsub('"', ''))
   end
 end
